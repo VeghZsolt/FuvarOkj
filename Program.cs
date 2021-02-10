237308f2b31d1d58538304a73eace20f123a9b53
VeghZsolt <78847631+VeghZsolt@users.noreply.github.com>
2021-02-10 11:46:39 +0100

    2. Feladat

--- Program.cs
+++ Program.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace Fuvar
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            String[] sorok = File.ReadAllLines("fuvar.csv");
 
         }
     }
