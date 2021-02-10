e872d96f275e343b7d5e9be62cbd4d28166a7ed7
VeghZsolt <78847631+VeghZsolt@users.noreply.github.com>
2021-02-10 13:05:00 +0100

    6. feladat

--- Program.cs
+++ Program.cs
@@ -18,7 +18,53 @@ namespace Fuvar
             }
 
             Console.WriteLine("3.feladat: " +fuvarok.Count + " fuvar");
-            
+
+
+            int db = 0;
+            double fiz = 0;
+            for (int i = 0; i < fuvarok.Count; i++)
+            {
+                if(fuvarok.ElementAt(i).ID == 6185)
+                {
+                    db++;
+                    fiz += fuvarok.ElementAt(i).Vitel;
+                    fiz += fuvarok.ElementAt(i).Borr;
+                }
+            }
+
+            Console.WriteLine($"4 feladat: {db} fuvar alatt: {fiz:N3}");
+
+
+            Dictionary<string, int> fizetesimodok = new Dictionary<string, int>();
+            foreach (Fuvar fuvar1 in fuvarok)
+            {
+                if(fizetesimodok.ContainsKey(fuvar1.Fizm))
+                {
+                    fizetesimodok[fuvar1.Fizm]++;
+                }
+                else
+                {
+                    fizetesimodok.Add(fuvar1.Fizm, 1);
+                }
+
+              
+
+            }
+            Console.WriteLine("5. feladat:");
+            foreach (KeyValuePair<string, int> item in fizetesimodok)
+            {
+                Console.WriteLine($"\t{item.Key}: {item.Value} fuvar");
+            }
+
+            double km = 0;
+            foreach (Fuvar fuvar in fuvarok)
+            {
+                
+                km += (fuvar.Megtett)/1.6;
+                
+            }
+
+            Console.WriteLine($"6. feladat: {km:N2}km");
             Console.ReadKey();
         }
     }
